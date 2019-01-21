925c8dbf3073a2bed94f59174150d7b0bed1b3a8
Mokky <a53312773@gmail.com>
2019-01-21 13:57:04 +0900

    Update exchange_rate.sh

--- exchange_rate.sh
+++ exchange_rate.sh
@@ -5,6 +5,19 @@ clear
 CACHE=$HOME/cache_exchange_rate
 DATE=$(date +%s)
 
+function CAL_EXCHANGE(){
+ORG_RATE=$(head -n $1 $CACHE/exchange-rate.info | tail -1)
+NEW_RATE=$(head -n $1 $CACHE/new_exchange-rate.info | tail -1)
+PRT_EX=$(printf %.2f $(echo "$ORG_RATE * $won" | bc))
+if [ "$EX_CHANGE" = 1 ]
+then
+EX_DIFF=$(echo "$NEW_RATE - $ORG_RATE" | bc)
+echo "$PRT_EX   |   $(printf %.2f $(echo "scale=2; $EX_DIFF / $ORG_RATE" | bc))%"
+else
+echo $PRT_EX
+fi
+}
+
 if [ ! -e $CACHE ]
 then
 mkdir -p $CACHE
@@ -12,26 +25,43 @@ fi
 
 echo -n "! 환율정보 불러오는 중..."
 
-curl -s "https://ko.exchange-rates.org/" | grep '"cross-rate"' | head -7 | cut -f2 -d'>' | sed 's/<br//g' > $CACHE/exchange-rate.info
+curl -s "https://ko.exchange-rates.org/" | grep '"cross-rate"' | head -7 | cut -f2 -d'>' | sed 's/<br//g' > $CACHE/new_exchange-rate.info
 
-echo " [완료]"
+if [ ! -e $CACHE/exchange-rate.info ]
+then 
+cp $CACHE/new_exchange-rate.info $CACHE/exchange-rate.info
+fi
+
+if [ "$(diff $CACHE/new_exchange-rate.info $CACHE/exchange-rate.info)" = "" ]
+then
+echo " [변동없음]"
+else
+echo " [변동]"
+EX_CHANGE=1
+fi
+
 echo
 read -p "! 금액 (단위: WON): " won
 
 echo "
 @ KRW (원): \\$won
 
-@ USD (달러): \$$(printf %.2f $(echo "$(head -1 $CACHE/exchange-rate.info | tail -1) * $won" | bc))
+@ USD (달러): \$$(CAL_EXCHANGE 1)
 
-@ EUR (유로): €$(printf %.2f $(echo "$(head -2 $CACHE/exchange-rate.info | tail -1) * $won" | bc))
+@ EUR (유로): €$(CAL_EXCHANGE 2)
 
-@ JPY (엔): ¥$(printf %.2f $(echo "$(head -3 $CACHE/exchange-rate.info | tail -1) * $won" | bc))
+@ JPY (엔): ¥$(CAL_EXCHANGE 3)
 
-@ GBP (파운드): £$(printf %.2f $(echo "$(head -4 $CACHE/exchange-rate.info | tail -1) * $won" | bc))
+@ GBP (파운드): £$(CAL_EXCHANGE 4)
 
-@ AUD (AU달러): AU\$$(printf %.2f $(echo "$(head -5 $CACHE/exchange-rate.info | tail -1) * $won" | bc))
+@ AUD (AU달러): AU\$$(CAL_EXCHANGE 5)
 
-@ HKD (HK달러): HK\$$(printf %.2f $(echo "$(head -6 $CACHE/exchange-rate.info | tail -1) * $won" | bc))
+@ HKD (HK달러): HK\$$(CAL_EXCHANGE 6)
 
-@ CAD (C달러): C\$$(printf %.2f $(echo "$(head -7 $CACHE/exchange-rate.info | tail -1) * $won" | bc))
+@ CAD (C달러): C\$$(CAL_EXCHANGE 7)
 "
+
+if [ "$EX_CHANGE" = 1 ]
+then
+mv $CACHE/new_exchange-rate.info $CACHE/exchange-rate.info
+fi
